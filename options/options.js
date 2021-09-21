190092fea5b2fc0bae0217a179e265fa4e1e4b68
Crowdbotics <team@crowdbotics.com>
2021-09-21 12:07:05 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "no_labels_counselin_30720Identifier",
+  url: "https://no_labels_counselin_30720Identifier.botics.co",
+  api: "https://no_labels_counselin_30720Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
